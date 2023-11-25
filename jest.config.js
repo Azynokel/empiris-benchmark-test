98710f0c3ee69cddea4bc570704602577a0eea50
Louis Loechel <57091589+louisloechel@users.noreply.github.com>
2023-11-25 19:49:29 +0000

    minor changes

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/inch.test.js'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'], // add this line
+};
